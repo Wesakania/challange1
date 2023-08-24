5732b135efd368df15ba9f38b380b10a4005eea1
Wesakania <wesakaniadavis@gmail.com>
2023-08-24 21:31:21 +0300

    myChallange on if/else

---
+++ index.js
@@ -0,0 +1,16 @@
+let day = "Sunday"
+
+if(day === "Moday"){
+    console.log("plan course structure");
+    console.log("Go to coding meetup");
+}else if(day === "Tuesday"){
+    console.log("prepare theory video");
+}else if(day === "Wednesday" || day ==="Thursday"){
+    console.log("Write code example")
+}else if (day === " Friday"){
+    console.log("Record Videos")
+}else if(day === "Saturday" || day === "Sunday"){
+    console.log("Enjoy the weekend")
+}else{
+    console.log("Not a valid day")
+}
